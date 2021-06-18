aebabe7c49bc697a63ccdc64d428b27c7977ea71
Ian Lai <ian24257587@gmail.com>
2021-06-18 13:13:49 +0800

    feat(core): build CoreMethod isCore

--- packages/highlighter-core/src/interfaces/core.ts
+++ packages/highlighter-core/src/interfaces/core.ts
@@ -1,22 +1,42 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import isPlainObject from 'is-plain-object';
 import type { Node, Core } from '../types';
 import { Key } from '../utils/key';
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+const IS_CORE_CACHE = new WeakMap<object, boolean>();
 
 export interface ICoreMethods {
   /**
    * Find a key for a Slate node.
    */
   findKey: (core: Core, node: Node) => Key;
-
-  /**
-   * Find the path of Slate node.
-   */
+  isCore: (value: any) => value is Core;
 }
 
 export const coreMethods: ICoreMethods = {
-  findKey(core: Core, node: Node) {
+  findKey(core: Core, node: Node): Key {
     let key;
 
     return key;
+  },
+  isCore(value: any): value is Core {
+    if (!isPlainObject(value)) return false;
+
+    const cachedIsCore = IS_CORE_CACHE.get(value);
+
+    if (cachedIsCore !== undefined) {
+      return cachedIsCore;
+    }
+
+    /**
+     * @todo
+     * CORE determinations
+     */
+    const isCore = true;
+
+    IS_CORE_CACHE.set(value, isCore);
+
+    return isCore;
   },
 };
--- packages/highlighter-react/src/components/highlighter.tsx
+++ packages/highlighter-react/src/components/highlighter.tsx
@@ -1,34 +1,29 @@
 import React, { useMemo } from 'react';
 import invariant from 'tiny-invariant';
-import type {} from 'highlighter-core';
+import type { IMethodsGateway, Core as HighlighterCore, Descendant } from 'highlighter-core';
 
 import { HighlighterCoreContext } from '../hooks';
 
-/**
- * @todo
- * MOCK types -> change to 'highlighter-core' package
- */
-import type { HighlighterCore, Descendant } from '../mock/interfaces';
-
 export interface HighlighterProps {
   core: HighlighterCore;
+  methods: IMethodsGateway;
   children: React.ReactNode;
   onChange: (value: Descendant[]) => void;
 }
 
 function Highlighter(props: HighlighterProps) {
   const {
-    core, children, onChange, ...rest
+    core, children, onChange, methods, ...rest
   } = props;
 
   const context: [HighlighterCore] = useMemo(() => {
     /** Type-Guard for core & value, using invariant */
-    invariant(core.commons.Editor.isEditor(core), `[Highlighter] core is invalid! you passed: ${JSON.stringify(core)}`);
+    invariant(methods('CORE', 'isCore')(core), `[Highlighter] core is invalid! you passed: ${JSON.stringify(core)}`);
 
     Object.assign(core, rest);
 
     return [core];
-  }, [core, rest]);
+  }, [core, rest, methods]);
 
   return <HighlighterCoreContext.Provider value={context}>{children}</HighlighterCoreContext.Provider>;
 }
